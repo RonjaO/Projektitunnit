7215857a6230411d6bd7a84ad6172a32b0c87904
RonjaO <ronja.oja@gmail.com>
2016-01-01 14:10:52 +0200

    SecurityConfiguration lisätty

---
+++ src/main/java/app/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/projektit/*").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+
+        http.formLogin().permitAll();
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            // käyttäjällä jack, jonka salasana on bauer, on rooli USER
+            auth.inMemoryAuthentication()
+                    .withUser("jack").password("bauer").roles("USER");
+        }
+    }
+}

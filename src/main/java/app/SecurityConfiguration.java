6b6e419960142e663b27a0205dd0420dfd682adf
RonjaO <ronja.oja@gmail.com>
2016-01-01 17:51:37 +0200

    ...

--- src/main/java/app/SecurityConfiguration.java
+++ src/main/java/app/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -23,6 +24,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.formLogin().loginPage("/").permitAll();
     }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Disable Spring Security completely on public static resources.
+        // This is the only way to avoid having "Cache-Control: must-revalidate" in the response, which
+        // both reduces responsiveness, and breaks IE's font loading.
+        web.ignoring().antMatchers("/public/**");     
+    }
+    
 
     @Configuration
     protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {

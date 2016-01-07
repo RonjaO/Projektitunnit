21218d228a6983cc9d03ff93d1ef6cbd110c8caa
RonjaO <ronja.oja@gmail.com>
2016-01-07 18:06:55 +0200

    ...

--- src/main/java/app/repository/TuntiRepository.java
+++ src/main/java/app/repository/TuntiRepository.java
@@ -40,7 +40,7 @@ public class TuntiRepository {
         LocalDateTime ldt = LocalDateTime.now();
         String alkuaika = "'" + ldt.toString() + "'";
         
-        String sql = "INSERT INTO Tunti(kayttaja, projekti_id, alkuaika) VALUES((SELECT id FROM Kayttaja WHERE email=?), ?, ?)";
+        String sql = "INSERT INTO Tunti(kayttaja, projekti_id, alkuaika) VALUES((SELECT id FROM Kayttaja WHERE email=?), ?, timestamp ?)";
         
         jdbc.update(sql, kayttaja, tunti.getProjektiId(), alkuaika);
     }

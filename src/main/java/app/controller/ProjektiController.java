09e7531ce0d23be6ef250a8be83ae081b2210dca
RonjaO <ronja.oja@gmail.com>
2016-01-07 22:39:32 +0200

    jeps

--- src/main/java/app/controller/ProjektiController.java
+++ src/main/java/app/controller/ProjektiController.java
@@ -25,6 +25,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import app.domain.Kayttaja; 
+import java.util.*;
 
 
 @Controller

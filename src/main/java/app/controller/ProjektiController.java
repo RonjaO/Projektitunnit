d827a6f13f6fc4162247c14276fdb8cb5a656245
RonjaO <ronja.oja@gmail.com>
2016-01-07 23:02:31 +0200

    jeps

--- src/main/java/app/controller/ProjektiController.java
+++ src/main/java/app/controller/ProjektiController.java
@@ -42,7 +42,7 @@ public class ProjektiController {
     public String view(Model model) {
         if (!(tunnit.kesken().isEmpty())) {
             List<Tunti> tunti = tunnit.kesken();
-            System.out.println("tunti: " + tunti.get(0).getId());
+            System.out.println("tunti: " + tunti.get(tunti.size() - 1).getId());
             model.addAttribute("tunti", tunti.get(0));
 
             int projekti = tunnit.kesken().get(0).getProjektiId();

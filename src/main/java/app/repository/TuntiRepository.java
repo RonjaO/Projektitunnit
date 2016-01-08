eb30664269dfcf553f923979019d527118c0deb0
RonjaO <ronja.oja@gmail.com>
2016-01-08 09:33:10 +0200

    ....

--- src/main/java/app/repository/TuntiRepository.java
+++ src/main/java/app/repository/TuntiRepository.java
@@ -60,7 +60,7 @@ public class TuntiRepository {
         jdbc.update(sql, loppuaika, tunti.getKuvaus(), tunti.getId());
 
          Duration kesto = tunti.getDuration();
-        projektit.lisaaTunti(tunti.getProjektiId(), kesto);
+        // projektit.lisaaTunti(tunti.getProjektiId(), kesto);
     } 
 
     public List<Tunti> kesken() {

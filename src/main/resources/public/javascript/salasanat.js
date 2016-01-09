ef885d90e90bf92edc9859015c3b37c5018df3de
RonjaO <ronja.oja@gmail.com>
2016-01-09 19:43:16 +0200

    JavaScriptiä vähäsen

--- src/main/resources/public/javascript/salasanat.js
+++ src/main/resources/public/javascript/salasanat.js
@@ -1,11 +1,17 @@
-var salasana = document.getElementById("salasana").value;
-var salasana2 = document.getElementById("salasana2").value;
 console.log("moi");
 
-if (salasana2 = "" ) {
-    document.getElementById("virhe").innerHTML = "";
-    console.log("Javascript tekee jotain");
-} else if (salasana != salasana2) {
-    document.getElementById("virhe").innerHTML = "Salasanat eivät tästmää";
-    console.log("Salasanoissa virhe");
+var salasana2 = document.getElementById("salasana2");
+
+salasana2.addEventListener('change', function(eventInformation) {
+    vertaile(salasana2.value);    
+}, false);
+
+function vertaile(salasana2) {
+    var salasana = document.getElementById("salasana").value;
+    
+
+    if (salasana != salasana2) {
+        document.getElementById("virhe").innerHTML = "Salasanat eivät tästmää";
+        console.log("Salasanoissa virhe, ") + salasana + " ja " + salasana2;
+    }   
 }

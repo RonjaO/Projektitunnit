caa73b04dcdfd5fde32821a616df5b1f0eb09328
RonjaO <ronja.oja@gmail.com>
2016-05-01 20:47:34 +0300

    jooh

--- src/main/java/app/profiles/DevProfile.java
+++ src/main/java/app/profiles/DevProfile.java
@@ -2,6 +2,7 @@ package app.profiles;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Bean;
 import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -12,12 +13,12 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @Profile(value = {"dev", "default"})
 public class DevProfile {
     
-    @Bean
-    public Datasource datasourse() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).addScript("sql/create_tables.sql").addScript("sql/add_test_data.sql").build();
-        
-        return db;
-    }
+    // @Bean
+    // public DataSource datasource() {
+    //     EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+    //     EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).addScript("sql/create_tables.sql").addScript("sql/add_test_data.sql").build();
+    //
+    //     return db;
+    // }
 
 }
---
+++ src/main/java/sql/add_test_data.sql
@@ -0,0 +1,8 @@
+InSERT INTO Kayttaja (nimi, email, password) VALUES ('Mikko', 'mikko@example.com', 'mikko1234');
+
+INSERT INTO Kayttaja (nimi, email, password) VALUES ('anna', 'anna@example.com', 'anna1234');
+
+INSERT INTO Projekti(nimi, kuvaus) VALUES ('Tietokantasovellus', 'aineopintojen harkkatyö');
+
+INSERT INTO Projekti(omistaja_kayttaja, nimi, kuvaus) VALUES (2, 'Matikka', 'Matematiikan tehtävät kurssilla xyy');
+

3a0bf4426b2932984f0b01c5268b3a4408e5ba31
RonjaO <ronja.oja@gmail.com>
2016-05-04 17:00:34 +0300

    Debuggausta

--- src/main/java/app/domain/Projekti.java
+++ src/main/java/app/domain/Projekti.java
@@ -88,7 +88,7 @@ public class Projekti {
     }
     
     public String getTunnit() {
-        System.out.println("Kesto on " + this.kesto.toString());
+        System.out.println("Kesto on " + this.kesto);
         String[] kaikkiTunnit = this.kesto.split(":");
         
         return kaikkiTunnit[0] + " h " + kaikkiTunnit[1] + " min";
